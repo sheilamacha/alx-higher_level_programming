e29b96636f55903677f3b1f48c78b9881ea99296
Sheila Macharia <111003634+sheilamacha@users.noreply.github.com>
2023-03-13 16:08:50 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-
-// Script that prints C is fun,Python is cool and JavaScript is amazing.
-
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
